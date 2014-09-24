746771fa1ca625ea0e8286e8fd2b25fc8a31b54a
Ian Jennings <ian@meetjennings.com>
2014-09-24 15:45:55 -0500

    add embedly dependency

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+var dotenv = require('dotenv');
 var PUBNUB = require('pubnub');
+var EMBEDLY_KEY = process.env.EMBEDLY_KEY;
+dotenv.load();
+
 
 var pubnub = PUBNUB.init({
   subscribe_key: 'sub-c-78806dd4-42a6-11e4-aed8-02ee2ddab7fe'
@@ -7,6 +11,6 @@ var pubnub = PUBNUB.init({
 pubnub.subscribe({
   channel: 'pubnub-twitter-spotify',
   message: function(tweet) { 
-    console.log(tweet);
+    console.log(tweet.text);
   }
 });
