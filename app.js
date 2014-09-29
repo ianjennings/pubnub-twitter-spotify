8faae4ffaf89ff8253a9ea7de0755dee0e7f2929
Ian Jennings <ian@meetjennings.com>
2014-09-29 16:59:35 -0500

    top tweeted spotify tracks

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ mongo.connect('mongodb://127.0.0.1:27017/pubnub-listening', function(err, db) {
   });
 
   app.get('/', function (req, res) {
-    res.render('index');
+    res.sendfile('index.html');
   });
 
   app.get('/top', function(req, res) {
